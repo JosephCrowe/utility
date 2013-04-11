383430bf1d09543a84d57a54f3f47879dceadc2d
Joseph Crowe <joseph.crowe@live.co.uk>
2013-04-11 12:18:27 +0100

    Update pipeserve.

--- pipeserve/pipeserve.c
+++ pipeserve/pipeserve.c
@@ -1,8 +1,12 @@
 /*******************************************************************************
  *  pipeserv accepts connections to a UNIX domain socket in the file system,
  *  forwarding each line of its stdin to each connected client, and each line
- *  received from any connected client to its stdout. Lines longer than BUFFER
- *  may be transmitted non-atomically, and hence mixed with other data.
+ *  received from any connected client to its stdout.
+ *
+ *  General information is printed to stderr.
+ *
+ *  Lines longer than BUFFER may be transmitted non-atomically, and hence mixed
+ *  with other data.
  */
 
 #define _GNU_SOURCE
@@ -21,39 +25,73 @@
 #include <sys/un.h>
 #include "list.h"
 
-#define UNIX_PATH_MAX 108
 
+#define UNIX_PATH_MAX 108
+#define LAMBDA(TYPE, BODY) ({ TYPE __lambda__ BODY __lambda__; })
+
+#define SIGRT_IO SIGRTMIN
 #define BACKLOG 16
-#define BUFFER 1024
-#define SIGRT_SERVER  (SIGRTMIN + 0)
-#define SIGRT_CLIENT  (SIGRTMIN + 1)
-#define SIGRT_STDIN   (SIGRTMIN + 2)
+#define BUFFER 10
 
 static int check(int);
 static ssize_t scheck(ssize_t);
-void atexit_func(void);
-void sigint(int, siginfo_t *, void *);
-void sigrt_server(int, siginfo_t *, void *);
-void sigrt_client(int, siginfo_t *, void *);
-void sigrt_stdin(int, siginfo_t *, void *);
 
+static void setup_io(int, int);
+static void handle_io(int, siginfo_t *, void *);
+
+static void server_io(void);
+static void client_io(int);
+static void stdin_io(void);
+static void child_io(int);
+
+char *name;
 char *socket_name;
+
+int server;
 list_t *clients;
+
+int child_in;
+int child_out;
+int child_err;
 
 char stdin_buffer[BUFFER];
 size_t stdin_bytes;
 
-int main(int argc, char **argv) {
+int main(int argc, char **argv, char *envp[]) {
     /* Read command-line arguments. */
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s SOCKET_NAME\n", basename(argv[0]));
+    char *_name = basename(argv[0]);
+    name = malloc(strlen(_name));
+    strcpy(name, _name);
+
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s SOCKET_NAME COMMAND...\n", name);
         return EXIT_FAILURE;
     }
+
     if (strlen(argv[1]) > UNIX_PATH_MAX) {
         fprintf(stderr, "Error: SOCKET_NAME is too long.\n");
         return EXIT_FAILURE;
     }
+
     socket_name = argv[1];
+
+    /* Start child process. */
+    int child_stdin[2], child_stdout[2], child_stderr[2];
+    check(pipe(child_stdin));
+    check(pipe(child_stdout));
+    check(pipe(child_stderr));
+
+    int child = check(fork());
+    if (child == 0) {
+        check(dup2(STDIN_FILENO, child_stdin[0]));
+        check(dup2(STDOUT_FILENO, child_stdout[1]));
+        check(dup2(STDERR_FILENO, child_stderr[1]));
+        check(execvpe(argv[2], &argv[2], envp));
+    }
+
+    child_in = child_stdin[1];
+    child_out = child_stdout[0];
+    child_err = child_stderr[0];
 
     /* Set up signal handlers. */
     sigset_t empty;
@@ -62,18 +100,12 @@ int main(int argc, char **argv) {
     struct sigaction action;
     action.sa_mask = empty;
     action.sa_flags = SA_SIGINFO;
+    action.sa_sigaction = &handle_io;
+    sigaction(SIGRT_IO, &action, NULL);
 
-    action.sa_sigaction = &sigint;
+    action.sa_flags = 0;
+    action.sa_handler = &exit;
     sigaction(SIGINT, &action, NULL);
-
-    action.sa_sigaction = &sigrt_server;
-    sigaction(SIGRT_SERVER, &action, NULL);
-
-    action.sa_sigaction = &sigrt_client;
-    sigaction(SIGRT_CLIENT, &action, NULL);
-
-    action.sa_sigaction = &sigrt_stdin;
-    sigaction(SIGRT_STDIN, &action, NULL);
 
     /* Set up listening socket. */
     clients = LIST_EMPTY;
@@ -82,57 +114,57 @@ int main(int argc, char **argv) {
     addr.sun_family = AF_UNIX;
     strcpy(addr.sun_path, socket_name);
 
-    int server = check(socket(AF_UNIX, SOCK_STREAM, 0));
+    server = check(socket(AF_UNIX, SOCK_STREAM, 0));
     check(bind(server, (struct sockaddr *) &addr, sizeof(struct sockaddr_un)));
-    atexit(&atexit_func);
-
-    check(fcntl(server, F_SETSIG, SIGRT_SERVER));
-    check(fcntl(server, F_SETFL, O_ASYNC | O_NONBLOCK));
-    check(fcntl(server, F_SETOWN, getpid()));
+    atexit(LAMBDA(void, (void) { unlink(socket_name); }));
+    setup_io(server, O_NONBLOCK);
     check(listen(server, BACKLOG));
 
     /* Set up stdin. */
     stdin_bytes = 0;
-    check(fcntl(STDIN_FILENO, F_SETSIG, SIGRT_STDIN));
-    check(fcntl(STDIN_FILENO, F_SETFL, O_ASYNC | O_NONBLOCK));
-    check(fcntl(STDIN_FILENO, F_SETOWN, getpid()));
+    setup_io(STDIN_FILENO, O_NONBLOCK);
 
     /* Wait for signals indefinitely. */
     for (;;) sigsuspend(&empty);
 }
 
-void sigrt_server (int signum, siginfo_t *info, void *context) {
-    int server = info->si_fd;
+void setup_io(int fd, int flags) {
+    check(fcntl(fd, F_SETSIG, SIGRT_IO));
+    check(fcntl(fd, F_SETFL, O_ASYNC | flags));
+    check(fcntl(fd, F_SETOWN, getpid()));
+}
+
+void handle_io(int signum, siginfo_t *info, void *context) {
+    int fd = info->si_fd;
+
+    if (fd == STDIN_FILENO) return stdin_io();
+    if (fd == server) return server_io();
+    if (fd == child_out || fd == child_err) return child_io(fd);
+
+    for (list_t *item = clients; item != NULL; item = item->tail)
+        if (fd == item->head) return client_io(fd);
+}
+
+void server_io (void) {
     for (;;) {
         int client = accept(server, NULL, 0);
         if (client == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) break;
         scheck(client);
-
-        check(fcntl(client, F_SETSIG, SIGRT_CLIENT));
-        check(fcntl(client, F_SETFL, O_ASYNC));
-        check(fcntl(client, F_SETOWN, getpid()));
-
+        setup_io(client, 0);
         clients = list_add(clients, client);
-        warnx("connect: %d", client);
+        fprintf(stderr, "%s: connect(%d)\n", name, client);
     }
 }
 
-void sigrt_client (int signum, siginfo_t *info, void *context) {
-    int client = info->si_fd;
-
-    list_t *item;
-    for (item = clients; item != NULL; item = item->tail)
-        if (item->head == client) break;
-    if (item == NULL) return;
-
+void client_io (int client) {
     for (;;) {
         char buffer[BUFFER];
 
-        ssize_t bytes = recv(client, (void *) &buffer, BUFFER,
-                             MSG_PEEK | MSG_DONTWAIT);
+        ssize_t bytes = recv(
+            client, (void *) &buffer, BUFFER, MSG_PEEK | MSG_DONTWAIT);
         if (bytes == -1 && (errno == EAGAIN || errno == EWOULDBLOCK)) break;
         if (bytes == 0 || bytes == -1) {
-            warnx("disconnect: %d", client);
+            fprintf(stderr, "%s: disconnect(%d)\n", name, client);
             close(client);
             clients = list_del(clients, client);
             break;
@@ -143,11 +175,16 @@ void sigrt_client (int signum, siginfo_t *info, void *context) {
         else if (bytes < BUFFER) continue;
 
         scheck(recv(client, (void *) buffer, bytes, 0));
-        scheck(write(STDOUT_FILENO, (void *) buffer, bytes));
+
+        fprintf(stderr, "%s(%d): ", name, client);
+        write(STDERR_FILENO, (void *) buffer, bytes);
+        if (buffer[bytes - 1] != '\n') fprintf(stderr, "\\\n");
+
+        write(STDOUT_FILENO, (void *) buffer, bytes);
     }
 }
 
-void sigrt_stdin(int signum, siginfo_t *info, void *context) {
+void stdin_io(void) {
     for (;;) {
         size_t remain = BUFFER - stdin_bytes;
         ssize_t bytes = read(STDIN_FILENO, (void *) &stdin_buffer + stdin_bytes,
@@ -170,8 +207,9 @@ void sigrt_stdin(int signum, siginfo_t *info, void *context) {
     }
 }
 
-void sigint (int signum, siginfo_t *info, void *context) {
-    exit(EXIT_SUCCESS);
+void child_io(int fd) {
+    /* fd is child_out or child_err. */
+    
 }
 
 static int check (int result) {
@@ -182,8 +220,4 @@ static int check (int result) {
 static ssize_t scheck (ssize_t result) {
     if (result == -1) err (EXIT_FAILURE, NULL);
     return result;
-}
-
-void atexit_func(void) {
-    unlink(socket_name);
 }
