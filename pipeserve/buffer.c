e7f9d1315d72749c4475453377d0a2e784b64ac7
Joseph Crowe <john.macinroe@gmail.com>
2014-05-04 07:48:02 +0100

    Fix bug in buffer.c when multiple lines arrive at once.

--- pipeserve/buffer.c
+++ pipeserve/buffer.c
@@ -37,34 +37,43 @@ ssize_t buffer_readline_r(int fd, char **line, size_t *rsize, char **rdata ) {
         buffers = plist_add(buffers, buffer);
     }
 
-    if (buffer->shift) {
-        memmove(&buffer->data[0], &buffer->data[buffer->shift],
-                buffer->end - &buffer->data[buffer->shift]);
-        buffer->end -= buffer->shift;
-        buffer->shift = 0;
-    }
-
     char *start = buffer->end;
     ssize_t bytes;
 
     for (;;) {
+        size_t dsize = buffer->end - &buffer->data[buffer->shift];
+        char *endl = memchr(&buffer->data[buffer->shift], '\n', dsize);
+        if (endl != NULL) {
+            // A line has been found within the buffer.
+            *line = &buffer->data[buffer->shift];
+            buffer->shift += bytes = 1 + endl - &buffer->data[buffer->shift];
+            break;
+        }
+
+        if (buffer->shift) {
+            memmove(&buffer->data[0], &buffer->data[buffer->shift],
+                    buffer->end - &buffer->data[buffer->shift]);
+            buffer->end -= buffer->shift;
+            start -= buffer->shift;
+            buffer->shift = 0;
+        }
+
         size_t space = BUFFER_SIZE - (buffer->end - &buffer->data[0]);
+        if (space == 0) {
+            // The buffer is full and no newline has been found.
+            *line = &buffer->data[0];
+            buffer->shift = bytes = BUFFER_SIZE;
+            break;
+        }
+
+        // The buffer is not yet full, and no newline has been found.
         bytes = read(buffer->fd, buffer->end, space);
-        if (bytes == -1 || bytes == 0) break;
-        char *endl = memchr(buffer->end, '\n', bytes);
+        if (bytes == -1) break;
         buffer->end += bytes;
-
-        if (endl == NULL && bytes < space) continue;
-
-        bytes = (endl == NULL) ? BUFFER_SIZE : (endl + 1 - &buffer->data[0]);
-        buffer->shift = bytes;
-        break;
     }
 
-    if (bytes != -1) *line = &buffer->data[0];
     if (rdata != NULL) *rdata = start;
     if (rsize != NULL) *rsize = buffer->end - start;
-
     return bytes;
 }
 
